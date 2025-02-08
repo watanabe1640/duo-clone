ea8a900897452611a3599831596cbe17ad7472ee
watanabe1640 <taiki6554@icloud.com>
2025-02-08 19:15:59 +0900

    shadcnui の追加とclerkの追加

---
+++ components/ui/button.tsx
@@ -0,0 +1,60 @@
+import * as React from "react"
+import { Slot } from "@radix-ui/react-slot"
+import { cva, type VariantProps } from "class-variance-authority"
+
+import { cn } from "@/lib/utils"
+
+const buttonVariants = cva(
+  "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-xl text-sm ring-offset-background transition-colors focus-visible:outline-none tracking-wide font-bold  focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0",
+  {
+    variants: {
+      variant: {
+        default: "bg-white text-black border-slate-200 border-2 border-b-[4px] hover:bg-slate-50 active:border-b-2 ",
+        primary: "bg-sky-500 text-primary-foreground hover:bg-sky-500/90 border-b-4 border-sky-600 active:border-b-0",
+        primaryOutline: "bg-white text-sky-500 hover:bg-slate-100",
+        secondary: "bg-green-500 text-primary-foreground hover:bg-green-500/90 border-b-4 border-green-600 active:border-b-0",
+        secondaryOutline: "bg-white text-green-500 hover:bg-slate-100",
+        danger: "bg-rose-500 text-primary-foreground hover:bg-rose-500/90 border-b-4 border-rose-600 active:border-b-0",
+        dangerOutline: "bg-white text-rose-500 hover:bg-slate-100",
+        super: "bg-indigo-500 text-primary-foreground hover:bg-indigo-500/90 border-b-4 border-indigo-600 active:border-b-0",
+        superOutline: "bg-white text-indigo-500 hover:bg-slate-100",
+        ghost: "bg-transparent text-slate-500 border-transparent border-0 hover:bg-slate-100",
+        sidebar: "bg-transparent text-slate-500 border-transparent border-2 hover:bg-slate-100 transition-none",
+        sidebarOutline: "bg-sky-500/15 text-sky-500 border-sky-300 border-2 hover:bg-sky-500/10 transition-none",
+      },
+      size: {
+        default: "h-11 px-4 py-2",
+        sm: "h-9 px-3",
+        lg: "h-12 px-8",
+        icon: "h-10 w-10",
+        rounded: "rounded-full",
+      },
+    },
+    defaultVariants: {
+      variant: "default",
+      size: "default",
+    },
+  }
+)
+
+export interface ButtonProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement>,
+    VariantProps<typeof buttonVariants> {
+  asChild?: boolean
+}
+
+const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
+  ({ className, variant, size, asChild = false, ...props }, ref) => {
+    const Comp = asChild ? Slot : "button"
+    return (
+      <Comp
+        className={cn(buttonVariants({ variant, size, className }))}
+        ref={ref}
+        {...props}
+      />
+    )
+  }
+)
+Button.displayName = "Button"
+
+export { Button, buttonVariants }
