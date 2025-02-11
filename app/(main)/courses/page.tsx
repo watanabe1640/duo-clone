16c966546c7b3662e3237d4f6073d9d28c9cd137
watanabe1640 <taiki6554@icloud.com>
2025-02-11 15:54:32 +0900

    ユーザー情報取得のためのコードを追加した。

--- app/(main)/courses/page.tsx
+++ app/(main)/courses/page.tsx
@@ -1,13 +1,20 @@
-import { getCourses } from "@/db/queries";
+import { getCourses, getUserProgress } from "@/db/queries";
 import { List } from "./list";
 export default async function CoursesPage() {
-	const courses = await getCourses();
+	const coursesPromise = getCourses();
+	const userProgressPromise = getUserProgress();
+
+	const [ courses, userProgress ] = await Promise.all([
+		coursesPromise,
+		userProgressPromise
+	])
+
 	return (
 		<>
 			<div className="h-full max-w-[912px] mx-auto px-3">
 				<h1 className="text-2xl font-bold text-neutral-700">
 					<List 
-						activeCourseId={1}
+						activeCourseId={userProgress?.activeCourseId}
 						courses={courses}
 					/>
 				</h1>
--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -2,8 +2,19 @@ import { StickyWrapper } from "@/components/sticky-wrapper";
 import { FeedWrapper } from "@/components/feed-wrapper";
 import { UserProgress } from "@/components/user-progress";
 import {Header} from "@/app/(main)/learn/header"
+import { getUserProgress } from "@/db/queries";
+import { redirect } from "next/navigation";
 
-export default function LearnPage() {
+export default async function LearnPage() {
+	const userProgressPromise = getUserProgress();
+
+	const [ userProgress ] = await Promise.all([
+		userProgressPromise
+	]);
+
+	if(!userProgress || !userProgress.activeCourse){
+		redirect("/courses");
+	}
 	return (
 		<>
 			<div className="flex flex-row-reverse gap-[48px] px-6">
