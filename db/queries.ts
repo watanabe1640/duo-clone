922ffbfba088fb449d26aa57e935f529654048ff
watanabe1640 <taiki6554@icloud.com>
2025-02-14 13:51:47 +0900

    seeding

--- db/queries.ts
+++ db/queries.ts
@@ -1,7 +1,7 @@
 import { cache } from "react"
 import db from "@/db/drizzle"
 import { eq } from "drizzle-orm";
-import { courses, userProgress } from "./schema";
+import { courses, userProgress, units } from "./schema";
 import { auth } from "@clerk/nextjs/server";
 
 export const getUserProgress = cache(async () => {
@@ -20,6 +20,42 @@ export const getUserProgress = cache(async () => {
 	return data;
 })
 
+export const getUnits = cache(async() => {
+	const userProgressData = await getUserProgress();
+
+	if (!userProgressData?.activeCourseId) {
+		return [];
+	}
+
+	const data = await db.query.units.findMany({
+		where: eq(units.courseId, userProgressData.activeCourseId),
+		with: {
+			lessons: {
+				with: {
+					challenges: {
+						with: {
+							challengeProgress:true,
+						}
+					}
+				}
+			}
+		}
+	});
+
+	const normalizedData = data.map((unit) => {
+		const lessonsWithCompletedStatus = unit.lessons.map((lesson) => {
+			const allCompletedChallenges = lesson.challenges.every((challenge) => {
+				return challenge.challengeProgress.length > 0 
+					&& challenge.challengeProgress
+					&& challenge.challengeProgress.every((progress) => progress.completed);
+			});
+			return {...lesson, completed: allCompletedChallenges};
+		});
+		return {...unit, lessons: lessonsWithCompletedStatus};
+	});
+	return normalizedData;
+});
+
 export const getCourses =  cache(async() => {
 	const data = await db.query.courses.findMany();
 
@@ -30,6 +66,5 @@ export const getCourseById = cache(async (courseId: number) => {
 	const data = await db.query.courses.findFirst({
 		where: eq(courses.id, courseId),
 	});
-	//todo: ここでコースのレッスンとユニットを取得したい
 	return data;
 });
--- scripts/seed.ts
+++ scripts/seed.ts
@@ -13,6 +13,11 @@ const main = async () => {
 
 		await db.delete(schema.courses);
 		await db.delete(schema.userProgress);
+		await db.delete(schema.units);
+		await db.delete(schema.lessons);
+		await db.delete(schema.challengeOptions);
+		await db.delete(schema.challengeProgress);
+		await db.delete(schema.challenges);
 
 		await db.insert(schema.courses).values([
 			{
@@ -30,7 +35,116 @@ const main = async () => {
 				title: "English",
 				imageSrc: "/images/GB.svg",
 			}
+		]);
+
+		await db.insert(schema.units).values([
+			{
+				id: 1,
+				courseId: 1,
+				title: "unit 1",
+				description: "Learn the basics of Japanese",
+				order: 1,
+
+			}
+		]);
+
+		await db.insert(schema.lessons).values([
+			{
+				id: 1,
+				unitId: 1, //"Learn the basics of Japanese"
+				title: "Nouns",
+				order: 1,
+			},
+			{
+				id: 2,
+				unitId: 1, //"Learn the basics of Japanese"
+				title: "Verbs",
+				order: 2,
+			},
+		]);
+		
+		await db.insert(schema.challenges).values([
+			{
+				id: 1,
+				lessonsId: 1, //Nouns
+				type: "SELECT",
+				question: "Which one of these is the 'りんご'?",
+				order: 1,
+			},
+			{
+				id: 2,
+				lessonsId: 1, //Nouns
+				type: "SELECT",
+				question: "Which one of these is the 'さかな'?",
+				order: 2,
+			},
+			{
+				id: 3,
+				lessonsId: 2, //Verbs
+				type: "SELECT",
+				question: "Which of these is a 'はしる'?",
+				order: 1,
+			},
+			{
+				id: 4,
+				lessonsId: 2, //Verbs
+				type: "SELECT",
+				question: "Which of these is a 'はねる'?",
+				order: 2,
+			}
+		]);
+
+		await db.insert(schema.challengeOptions).values([
+			{
+				id: 1,
+				challengeId: 1,
+				text: "りんご",
+				correct: true,
+			},
+			{
+				id: 2,
+				challengeId: 1,
+				text: "ばなな",
+				correct: false,
+			},
+			{
+				id: 3,
+				challengeId: 2,
+				text: "さかな",
+				correct: true,
+			},
+			{
+				id: 4,
+				challengeId: 2,
+				text: "くるま",
+				correct: false,
+			},
+			{
+				id: 5,
+				challengeId: 3,
+				text: "run",
+				correct: true,
+			},
+			{
+				id: 6,
+				challengeId: 3,
+				text: "push",
+				correct: false,
+			},
+			{
+				id: 7,
+				challengeId: 4,
+				text: "jump",
+				correct: true,
+			},
+			{
+				id: 8,
+				challengeId: 4,
+				text: "sit",
+				correct: false,
+			}
 		])
+
 		console.log("seeding finished");
 	}catch(error){
 		console.error(error);
